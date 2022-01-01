dc91cad6509cab925b50ab3711f6301931efa8e8
bharat3007 <96946875+bharat3007@users.noreply.github.com>
2022-01-02 01:56:25 +0530

    Update Book

--- src/main/java/com/myengine/product/Book.java
+++ src/main/java/com/myengine/product/Book.java
@@ -1,5 +1,11 @@
 package com.myengine.product;
 
-public class Book {
+public class Book extends Product{
+    
+    @Override
+    public String orderAction(){
+	return "PackingslipAction, RoyaltyOrderAction";
+    }
+
 
 }
