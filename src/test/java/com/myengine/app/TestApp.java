61b01ed928728d01f0b904631468c94babb15f8c
bharat3007 <96946875+bharat3007@users.noreply.github.com>
2022-01-02 02:14:55 +0530

    update test case

--- src/test/java/com/myengine/app/TestApp.java
+++ src/test/java/com/myengine/app/TestApp.java
@@ -1,5 +1,20 @@
 package com.myengine.app;
 
-public class TestApp {
 
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.myengine.product.Book;
+
+public class TestApp {
+	
+	 @Test
+	    public void PhysicalProductShouldGeneratePacking() {
+		Book thisBook = new Book();
+		String actionList = thisBook.orderAction();
+		assertEquals(actionList, "PackingslipAction, RoyaltyOrderAction");
+		
+	    }
+	   
+	   
 }
