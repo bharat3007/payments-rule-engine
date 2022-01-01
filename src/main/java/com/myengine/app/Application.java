5902d1321994f63ed5112b2d7d083fc916c58b16
bharat3007 <96946875+bharat3007@users.noreply.github.com>
2022-01-02 01:50:35 +0530

    Update main class file

--- src/main/java/com/myengine/app/Application.java
+++ src/main/java/com/myengine/app/Application.java
@@ -1,5 +1,34 @@
 package com.myengine.app;
 
+import com.myengine.product.Book;
+import com.myengine.product.Product;
+
 public class Application {
+	 
+    public static void main( String[] args )
+    {
+	Book myBook = new Book();
+	
+	System.out.println( "Hello World!" );
+    }
+
+    public String GeneratePackingSlip(Product product){
+
+	return "";
+    }
+    public String GenerateRoyaltyPackingSlip(Product product) {
+
+	return "";
+    }
+    
+    public void ActivateMembership() {
+
+    }
+
+    public void UpgradeMemberhip() {
+    }
+
+    public void SendMembershipActionEmail() {
+    }
 
 }
