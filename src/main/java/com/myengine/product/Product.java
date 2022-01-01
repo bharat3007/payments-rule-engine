b0f26ba8440f3bb33faa742d9ceb653947e8f68b
bharat3007 <96946875+bharat3007@users.noreply.github.com>
2022-01-02 01:56:11 +0530

    updated Product

--- src/main/java/com/myengine/product/Product.java
+++ src/main/java/com/myengine/product/Product.java
@@ -1,5 +1,14 @@
 package com.myengine.product;
 
 public class Product {
+	 private boolean isPhysicalProduct = false;
+	    private ProductType prodType;
+	    public boolean isPhysical( ){
+		return isPhysicalProduct;
+	    }
+	    
+	    public String orderAction(){
+		return "DefaultOrderAction";
+	    }
 
 }
